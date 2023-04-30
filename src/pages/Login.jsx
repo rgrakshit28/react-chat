5e782148b398fb263c83ea5ebe01cf1a3ba0740f
rgrakshit28 <rgrakshit28.professional@gmail.com>
2023-04-30 05:39:34 +0530

    Added Login and Sign up Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { useState } from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  Link,
+  useToast,
+} from "@chakra-ui/react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { email, password };
+      await axios.post("/api/user/login", body);
+      navigate("/");
+    } catch (error) {
+      toast({ title: error.response.data?.message, status: "error", duration: 3000 });
+    }
+  };
+
+  return (
+    <Flex height={"100vh"} align={"center"} justify={"center"} bg={"#1B1C24"}>
+      <Stack width={{ base: "70vw", md: "50vw", lg: "30vw" }} spacing="5">
+        <Stack align={"center"} py="5">
+          <Heading fontSize={"4xl"} color={"white"}>Login</Heading>
+        </Stack>
+        <Box rounded={"xl"} bg={"white"} p={8}>
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" isRequired value={email} onChange={(e) => setEmail(e.target.value)} />
+              </FormControl>
+
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" isRequired value={password} onChange={(e) => setPassword(e.target.value)} />
+              </FormControl>
+            </Stack>
+            <Stack mt={5}>
+              <Button type="submit" bg={"blue.500"} color={"white"} _hover={{ bg: "blue.900" }}>
+                Login
+              </Button>
+            </Stack>
+          </form >
+          <Stack spacing={4} mt={5}>
+            <Text fontSize={"md"} color={"gray.500"} align={"center"}>
+              Dont have an account?{" "}
+              <Link as={RouterLink} to="/signup" color={"blue.500"}>
+                Signup
+              </Link>
+            </Text>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import { useState } from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  Link,
+  useToast,
+} from "@chakra-ui/react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
-  return <div>Signup</div>;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { name, email, password };
+      await axios.post("/api/user/signup", body);
+      navigate("/");
+    } catch (error) {
+      toast({ title: error.response.data?.message, status: "error", duration: 3000 });
+    }
+  };
+
+  return (
+    <Flex height={"100vh"} align={"center"} justify={"center"} bg={"#1B1C24"}>
+      <Stack width={{ base: "70vw", md: "50vw", lg: "30vw" }} spacing="5">
+        <Stack align={"center"} py="5">
+          <Heading fontSize={"4xl"} color={"white"}>Sign up</Heading>
+        </Stack>
+        <Box rounded={"xl"} bg={"white"} p={8}>
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+              <FormControl id="name">
+                <FormLabel>Full Name</FormLabel>
+                <Input type="text" isRequired value={name} onChange={(e) => setName(e.target.value)} />
+              </FormControl>
+
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" isRequired value={email} onChange={(e) => setEmail(e.target.value)} />
+              </FormControl>
+
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" isRequired value={password} onChange={(e) => setPassword(e.target.value)} />
+              </FormControl>
+            </Stack>
+            <Stack mt={5}>
+              <Button type="submit" bg={"blue.500"} color={"white"} _hover={{ bg: "blue.900" }}>
+                Sign up
+              </Button>
+            </Stack>
+          </form >
+          <Stack spacing={4} mt={5}>
+            <Text fontSize={"md"} color={"gray.500"} align={"center"}>
+              Already have an account?{" "}
+              <Link as={RouterLink} to="/login" color={"blue.500"}>
+                Login
+              </Link>
+            </Text>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Signup;
